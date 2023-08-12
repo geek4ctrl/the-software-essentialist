5294c2c136ce957548d49a3d6b3eb16b0eda7028
Laurent Mbuyu <LMbuyu@osiristrading.com>
2023-08-12 12:43:15 +0200

    [New]: Get a user based on email has been added.

--- 0_From_Code_First_To_First_Principles/1_code_first/assignment/src/controllers/users.ts
+++ 0_From_Code_First_To_First_Principles/1_code_first/assignment/src/controllers/users.ts
@@ -20,3 +20,27 @@ const getUsers = async (req: Request, res: Response, next: NextFunction) => {
 
 }
 
+const getUser = async (req: Request, res: Response, next: NextFunction) => {
+
+    let email: string = req.params.email;
+
+    let result: AxiosResponse = await axios.get(`https://jsonplaceholder.typicode.com/users/${email}`);
+    let user: User = result.data;
+
+    if (user) {
+        return res.status(200).json({
+            error: undefined,
+            data: user,
+            success: true
+        });
+    } else {
+        return res.status(404).json({
+            error: "UserNotFound",
+            data: undefined,
+            success: false
+        });
+    }
+
+
+
+}
